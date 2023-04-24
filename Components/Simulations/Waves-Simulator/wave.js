60e580405cf7bb2d87d413575a381dbdd221717c
JAYESHBATRA <112322302+JAYESHBATRA@users.noreply.github.com>
2023-04-24 23:46:44 +0530

    Version 1.0
    Experience immersive learning like never before with simulations, 3D visualizations, customized quizzes, and videos all in one place !

---
+++ Components/Simulations/Waves-Simulator/wave.js
@@ -0,0 +1,210 @@
+var c = document.getElementById('screen');
+var b = document.getElementById('btn');
+var rst = document.getElementById('rst');
+
+var a,T,v; // Variables for holding wave parameters
+var t = 0,flag = 1,loop;
+var fun = sin; // Wave function
+
+var scr = c.getContext('2d');
+var maxx = c.width;
+var maxy = c.height;
+
+/* Initialises the screen with the axes and the wave */
+function initscr() {
+    /* Initial styles */
+    scr.font = '20px sans-serif';
+    scr.textAlign = 'center';
+    scr.strokeStyle = '#000000';
+    scr.fillStyle = '#000000';
+
+    /* y-axis */
+    scr.beginPath();
+    scr.moveTo(50,maxy/6);
+    scr.lineTo(50,maxy*5/6);
+    scr.stroke();
+    scr.fillText('y',50,maxy/6 - 15);
+
+    /* x-axis */
+    scr.beginPath();
+    scr.moveTo(50,maxy/2);
+    scr.lineTo(maxx-50,maxy/2);
+    scr.stroke();
+    scr.fillText('x',maxx-35,maxy/2 + 6);
+    scr.fillText('S',20,maxy/2 + 7); // Source label
+    
+    /* Draw the arrow tips at ends of each axes */
+                                    // axis: y = 1 || x = -1.
+    drawArrow(50,maxy/6,1,1);       // ori = 1 for upright(in y) and
+    drawArrow(50,maxy*5/6,1,-1);    // right-pointing(in x) arrows.
+    drawArrow(maxx-50,maxy/2,-1,1); // ori = -1 for upside-down(in y)
+                                    // and left-pointing(in x) arrows
+    /* Axes values */
+    scr.font = '14px sans-serif';
+    scr.fillStyle = '#666666';
+    scr.strokeStyle = '#666666';
+
+    for (i=1; i<=(maxx-120)/50; i++) {
+        scr.fillText(i*50,i*50+50,maxy/2 + 20);
+        scr.moveTo(i*50+50, maxy/2 - 5);
+        scr.lineTo(i*50+50, maxy/2 + 5);
+        scr.stroke();
+    }
+    scr.textAlign = 'right';
+    for (i=-2; i<=2; i++) {
+        scr.fillText(i*50,40,maxy/2 - i*50 + 5);
+        scr.moveTo(45, maxy/2 - i*50);
+        scr.lineTo(55, maxy/2 - i*50);
+        scr.stroke();
+    }
+    scr.moveTo(50,maxy/2); // Move cursor to the origin of the axes
+}
+
+/* Function to draw an arrow-tip in given axis and orientation */
+function drawArrow(xPos,yPos,axis,ori) {
+    scr.beginPath();
+    
+    scr.moveTo(xPos - ori*8, yPos + ori*axis*8);
+    scr.lineTo(xPos,yPos);
+    scr.lineTo(xPos + ori*axis*8, yPos + ori*8);
+    
+    scr.stroke();
+}
+
+/* Function to update the values of a,T and v */ 
+function updateParameters () {
+    a = parseFloat(document.getElementById('amp').value);
+    T = parseFloat(document.getElementById('prd').value);
+    v = parseFloat(document.getElementById('vel').value);
+    document.getElementById('amp-val').innerHTML = a.toFixed(0)
+    document.getElementById('prd-val').innerHTML = T.toFixed(2)
+    document.getElementById('vel-val').innerHTML = v.toFixed(0)
+}
+
+/* Function to draw the full wave at a given instance of time */
+function drawWave(time, d) {
+    var x,y;
+    updateParameters();
+    
+    scr.strokeStyle = '#203681'; // Set color for drawing the wave
+    scr.beginPath();             // Begins the wave
+
+    for(x=0; x < maxx-100 && x < d; x++) {       
+        y = fun(x, time);
+        scr.lineTo(50+x,maxy/2-y);
+    }
+    
+    scr.stroke(); // Stroke the wave on the screen
+
+    // Draw oscillating source point
+    scr.fillStyle = '#203681'; // Set color for drawing the wave
+    scr.beginPath();             // Begins the wave
+    scr.arc(50, maxy/2-fun(0, time), 3, 0, 2*Math.PI);
+    scr.fill();
+
+    // Display global time
+    scr.font = '16px monospace';
+    scr.textAlign = 'left';
+    scr.fillText('Time: ' + t.toFixed(2) + 's', 45, maxy-20);
+}
+
+/* Functon to clear the screen */
+function clrscr() {
+    scr.fillStyle = "#ffffff";
+    scr.fillRect(0,0,maxx,maxy);
+    scr.fillStyle = "#f24200"; // Resets the fill color
+}
+
+/* Update the wave on changing the parameter-inputs */
+function updateWave() {
+    clrscr();
+    initscr();
+    if (t) {
+        drawWave(t, v*t);
+    } else {
+        drawWave(t, 150);
+    }
+}
+for(i=0; i<3; i++) {
+    document.querySelectorAll('input')[i].oninput = updateWave;
+}
+document.getElementById('fun').onchange = function() {
+    switch (this.value) {
+        case 'Saw Tooth':
+            fun = saw;
+            break;
+        case 'Triangular':
+            fun = tri;
+            break;
+        case 'Square':
+            fun = sqr;
+            break;
+        case 'Sine':
+        default:
+            fun = sin;
+            break;
+    }
+    updateWave();
+}
+
+/* Function to Start/Pause the wave */
+function startWave() {
+    if(flag) {
+        loop = setInterval(function() {
+            clrscr();
+            initscr();
+            drawWave(t, v*t);
+            t = t+0.02; // Frame rate = 50fps; 1s/50 = 0.02s
+        }, 20); // 50fps x 20ms = 1000ms = 1s
+        
+        b.innerHTML = 'Pause';
+        flag = 0;
+    }
+    else {
+        clearInterval(loop);
+        b.innerHTML = 'Start';
+        flag = 1;
+    }
+}
+
+/* Initialise the screen for the first time */
+function reset() {
+    if (!flag) {
+        startWave(); // Pause the wave
+    }
+    t = 0;
+    clrscr();
+    initscr();
+
+    document.getElementById('amp').value = 50;
+    document.getElementById('prd').value = 1;
+    document.getElementById('vel').value = 150;
+    document.getElementById('fun').selectedIndex = 0;
+    fun = sin;
+    drawWave(1, 150);
+}
+rst.onclick = reset;
+reset();
+
+/* Start the wave when user clicks the 'Start' button */
+b.onclick = function() {
+    startWave();
+};
+
+
+
+/* Various wave functions */
+function sin(x, t) {
+    return a * Math.sin(2*Math.PI * (t-x/v)/T); // Standard eq of a sine wave
+}
+function saw(x, t) {
+    return 2*a * ((t-x/v)/T - Math.floor((t-x/v)/T) - 1/2);
+}
+function sqr(x, t) {
+    return a * Math.pow(-1, Math.floor((t-x/v) / T));
+}
+function tri(x, t) {
+    return -2 * (Math.abs(saw(x,t)) - a/2);
+}
+
+
---
+++ Pages/Simulation/Interactions/Button-3/sketch.js
@@ -0,0 +1,154 @@
+let b1, b2;
+let e = 1;
+let button1, button2, button3, slider, button4;
+let slider_m1, slider_m2, slider_u1, slider_u2;
+let x, y;
+let ball_u1 = 10, ball_u2 = 0, ball_m1 = 100, ball_m2 = 100;
+
+let posx = 200;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+	if(windowWidth<510){
+		slider = createSlider(0, 1, e, 0.01);
+		slider.position(width*(0.07), height*(1.10));
+	}
+	else{
+		slider = createSlider(0, 1, e, 0.01);
+		slider.position(width*(0.02), height*(0.42));
+	}
+	slider_m1 = createSlider(50, 200, ball_m1, 10);
+	slider_m1.position(width - posx, height*(0.29));
+
+	slider_m2 = createSlider(50, 200, ball_m2, 10);
+	slider_m2.position(width - posx, height*(0.325));
+
+	slider_u1 = createSlider(0, 10, ball_u1, 1);
+	slider_u1.position(width - posx, height*(0.360));
+
+	slider_u2 = createSlider(0, 5, ball_u2, 1);
+	slider_u2.position(width - posx, height*(0.395));
+
+	x = 100;
+	y = height / 2;
+	
+	ball_m1 = slider_m1.value();
+	ball_m2 = slider_m2.value();
+
+	ball_u1 = slider_u1.value();
+	ball_u2 = slider_u2.value();
+
+	c1 = color(255, 0, 0);
+	c2 = color(51);
+
+	b1 = new Ball(ball_m1/5, y, ball_m1, ball_u1, c1);
+	if(windowWidth>500)
+		b2 = new Ball(x + 300, y, ball_m2, ball_u2, c2);
+	else
+	b2 = new Ball(x + 80, y, ball_m2, ball_u2, c2);
+	frameRate(27);
+}
+
+function draw() {
+	background(250);
+	text("e = " + slider.value(), width/2, 200);
+
+	textSize(15);
+	let p = 140;
+	text("m1", width - posx + p, 20);
+	text("m2", width - posx + p, 40);
+	text("u1", width - posx + p, 60);
+	text("u2", width - posx + p, 80);
+
+	text(slider_m1.value() + " kg", width - posx - 60, 20);
+	text(slider_m2.value() + " kg", width - posx - 60, 40);
+	text(slider_u1.value() + " m/s", width - posx - 60, 60);
+	text(slider_u2.value() + " m/s", width - posx - 60, 80);
+
+	b1.createParticle();
+	b2.createParticle();
+
+	b1.move();
+	b2.move();
+	b1.collide(b2);
+  
+}
+
+function changeE0() {
+	changeE3();
+	e = 0;
+	removeSliders();
+	setup();
+}
+function changeE1() {
+	changeE3();
+	e = 1;
+	removeSliders();
+	setup();
+}
+function changeE2() {
+	changeE3();
+	e = slider.value();
+	removeSliders();
+	setup();
+}
+
+function changeE3() {
+	ball_m1 = slider_m1.value()
+	ball_m2 = slider_m2.value()
+	ball_u1 = slider_u1.value()
+	ball_u2 = slider_u2.value()
+	e = slider.value();
+	removeSliders();
+	setup();
+}
+
+function removeSliders() {
+	slider_m1.remove();
+	slider_m2.remove();
+	slider_u1.remove();
+	slider_u2.remove();
+	slider.remove();
+}
+
+class Ball {
+	constructor(x, y, m, xSpeed, c) {
+		this.x = x,
+		this.y = y,
+        this.m = m;
+        this.r = m / 5;
+		this.xSpeed = xSpeed;
+		this.c = c;
+		
+	}
+	createParticle() {
+		noStroke();
+		fill(this.c);
+		ellipse(this.x, this.y, this.r * 2);
+		textSize(15);
+		text(this.xSpeed + " m/s", this.x - this.r, this.y - this.r - 40);
+		text(this.m + " kg", this.x - this.r, this.y + this.r + 40);
+		textSize(32);
+	}
+	move() {
+		this.x += this.xSpeed;
+	}
+	update(newSpeed) {
+		this.xSpeed = newSpeed;
+	}
+	collide(other) {
+		if (this.x + this.r >= other.x - other.r) {
+			let up1 = this.xSpeed * (this.m - other.m * e) + other.m * other.xSpeed * (1 + e);
+			let low1 = this.m + other.m;
+			let result1 = up1 / low1;
+
+			let up2 = this.m * this.xSpeed * (1 + e) + other.xSpeed * (other.m - this.m * e);
+			let low2 = this.m + other.m;
+			let result2 = up2 / low2;
+
+			this.update(result1);
+			other.update(result2);
+		}
+	}
+}
