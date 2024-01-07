556a4ebbd5dbf37d627c204b033c4d89ceb39f95
yashi-15 <sharmayashi270@gmail.com>
2024-01-07 13:18:45 +0530

    animation in all pages

--- scroll-reveal.js
+++ scroll-reveal.js
@@ -8,25 +8,37 @@ ScrollReveal({
 // Customisation of Scroll Reveal rules for different classes
 
 ScrollReveal().reveal(
-	".main-nav,#sub-para,.primary-button,.features-heading-text",
+	".main-nav,#sub-para,.primary-button,.features-heading-text,.head-txt,.para-1,.card-1",
 	{
 		delay: 200,
 		origin: "top",
 	}
 );
-ScrollReveal().reveal(".features-card,.popup-premium", {
+ScrollReveal().reveal(".features-card,.popup-premium,.para-2,.card-2", {
 	delay: 300,
 	origin: "top",
 });
-ScrollReveal().reveal(".popup-basic,.popup-ultimate", {
+ScrollReveal().reveal(".para-3,.card-3", {
+	delay: 400,
+	origin: "top",
+});
+ScrollReveal().reveal(".popup-basic,.popup-ultimate,.para-4,.card-4", {
 	delay: 500,
 	origin: "top",
 });
-ScrollReveal().reveal(".hero-image,.right,.feature-image-right", {
+ScrollReveal().reveal(".para-5,.card-5", {
+	delay: 600,
+	origin: "top",
+});
+ScrollReveal().reveal(".card-6", {
+	delay: 700,
+	origin: "top",
+});
+ScrollReveal().reveal(".hero-image,.right,.feature-image-right,.head-img", {
 	delay: 200,
 	origin: "right",
 });
-ScrollReveal().reveal(".feature-image-left,.left", {
+ScrollReveal().reveal(".feature-image-left,.left,.button-10", {
 	delay: 200,
 	origin: "left",
 });
