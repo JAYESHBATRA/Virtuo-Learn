77fded26fb19f5b83187fa15403a5739e9f4e2e0
paulkrishanu4 <paulkrishanu84@gmail.com>
2024-06-08 11:36:06 +0530

    scroll down progress bar added

--- scroll-reveal.js
+++ scroll-reveal.js
@@ -46,3 +46,10 @@ ScrollReveal().reveal(".footer-content", {
 	delay: 200,
 	origin: "bottom",
 });
+let progressBar = document.querySelector(".progress-bar");
+let documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+ 
+window.onscroll = function(){
+   let progress = (scrollY / documentHeight) * 100;
+   progressBar.style.width = progress + "%";
+}
